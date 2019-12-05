0e3b49449ffd5145f27e9fd7748a70479dcd6a09
songtoan0201 <songtoan0201@gmail.com>
2019-12-05 15:19:44 -0800

    print activities in reversed order

--- src/main/java/com/codingdojo/ninjagold/HomeController.java
+++ src/main/java/com/codingdojo/ninjagold/HomeController.java
@@ -3,7 +3,9 @@ package com.codingdojo.ninjagold;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 import java.util.Random;
 
 import javax.servlet.http.HttpServletRequest;
@@ -24,7 +26,7 @@ public class HomeController {
 	}
 
 	@RequestMapping(value = "/process_money", method = RequestMethod.POST)
-	public String secretCode(@RequestParam String which_form, HttpSession session, HttpServletRequest request) {
+	public String processMoney(@RequestParam String which_form, HttpSession session, HttpServletRequest request) {
 		ArrayList<String> strArr = null;
 		if (session.getAttribute("activities") == null) {
 			strArr = new ArrayList<String>();
@@ -39,35 +41,34 @@ public class HomeController {
 		if(request.getParameter("which_form").equals("farm")) {
 			session.setAttribute("farm_gold", r.nextInt(11) + 10 );	
 			session.setAttribute("total_gold", (int)session.getAttribute("total_gold") + (int)session.getAttribute("farm_gold"));		
-			strArr.add(String.format("<option style='color:green'>Earn %d from farming (%s)</option>", session.getAttribute("farm_gold"), session.getAttribute("dateFormat") ));
-			session.setAttribute("activities", strArr);		
+			strArr.add(0,String.format("<option style='color:green'>Earn %d from farming (%s)</option>", session.getAttribute("farm_gold"), session.getAttribute("dateFormat") ));
+			session.setAttribute("activities", strArr);	
 		} else if(request.getParameter("which_form").equals("cave")) {
 			session.setAttribute("cave_gold", r.nextInt(6) + 5 );	
 			session.setAttribute("total_gold", (int)session.getAttribute("total_gold") + (int)session.getAttribute("cave_gold"));		
-			strArr.add(String.format("<option style='color:green'>Earn %d from farming (%s)</option>", session.getAttribute("cave_gold"), session.getAttribute("dateFormat") ));
+			strArr.add(0,String.format("<option style='color:green'>Earn %d from digging cave (%s)</option>", session.getAttribute("cave_gold"), session.getAttribute("dateFormat") ));
 			session.setAttribute("activities", strArr);		
 		} else if(request.getParameter("which_form").equals("house")) {
 			session.setAttribute("house_gold", r.nextInt(6) + 5 );	
 			session.setAttribute("total_gold", (int)session.getAttribute("total_gold") + (int)session.getAttribute("house_gold"));		
-			strArr.add(String.format("<option style='color:green'>Earn %d from farming (%s)</option>", session.getAttribute("house_gold"), session.getAttribute("dateFormat") ));
+			strArr.add(0,String.format("<option style='color:green'>Earn %d from searching houses (%s)</option>", session.getAttribute("house_gold"), session.getAttribute("dateFormat") ));
 			session.setAttribute("activities", strArr);		
 		} else if(request.getParameter("which_form").equals("casino")) {
 			if ((int)session.getAttribute("total_gold") > 0) {
 				session.setAttribute("casino_gold", r.nextInt(101) - 50);	
 				session.setAttribute("total_gold", (int)session.getAttribute("total_gold") + (int)session.getAttribute("casino_gold"));		
 				if ((int)session.getAttribute("casino_gold") < 0) {
-					strArr.add(String.format("<option style='color:red'>Lose %d from farming (%s)</option>", -(int)session.getAttribute("casino_gold"), session.getAttribute("dateFormat") ));
+					strArr.add(0,String.format("<option style='color:red'>Lose %d from gambling (%s)</option>", -(int)session.getAttribute("casino_gold"), session.getAttribute("dateFormat") ));
 					session.setAttribute("activities", strArr);		
 				}
 				else {
-					strArr.add(String.format("<option style='color:green'>Earn %d from farming (%s)</option>", session.getAttribute("casino_gold"), session.getAttribute("dateFormat") ));
+					strArr.add(0,String.format("<option style='color:green'>Earn %d from gambling (%s)</option>", session.getAttribute("casino_gold"), session.getAttribute("dateFormat") ));
 					session.setAttribute("activities", strArr);	
 				}
 			} else {
-				strArr.add(String.format("<option style='color:red'>You have no money to enter casino (%s)</option>", session.getAttribute("dateFormat") ));
+				strArr.add(0,String.format("<option style='color:red'>You have no money to enter casino (%s)</option>", session.getAttribute("dateFormat") ));
 				session.setAttribute("activities", strArr);	
-			}
-				
+			}			
 		}
 //		System.out.println("------------------");
 //		System.out.println(session.getAttribute("activities"));
